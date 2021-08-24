e018c89adebc189e990c82d7340f13e0784db322
Laisses <71999249+Laisses@users.noreply.github.com>
2021-08-24 14:34:26 -0300

    index.js: troca var por let.

--- client/app/index.js
+++ client/app/index.js
@@ -1,5 +1,5 @@
 
-var campos = [
+let campos = [
     document.querySelector('#data'),
     document.querySelector('#valor'),
     document.querySelector('#quantidade')
@@ -7,20 +7,20 @@ var campos = [
 
 console.log(campos);
 
-var tbody = document.querySelector('table tbody');
+let tbody = document.querySelector('table tbody');
 
 document.querySelector('.form').addEventListener('submit', function (event) {
 
     event.preventDefault();
-    var tr = document.createElement('tr');
+    let tr = document.createElement('tr');
 
     campos.forEach(function (campo) {
-        var td = document.createElement('td');
+        let td = document.createElement('td');
         td.textContent = campo.value;
         tr.appendChild(td);
     });
 
-    var tdVolume = document.createElement('td');
+    let tdVolume = document.createElement('td');
     tdVolume.textContent = campos[1].value * campos[2].value;
     tr.appendChild(tdVolume);
 
